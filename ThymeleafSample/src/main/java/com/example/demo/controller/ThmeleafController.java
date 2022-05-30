d6b8188ed32f017ab0d6fb30329950f6a1d15b00
yukihiro okada <save.live.wyldelife@gmail.com>
2022-05-30 17:42:25 +0900

    P178まで Thymeleaf3.0のfragmentが動作しない

--- ThymeleafSample/src/main/java/com/example/demo/controller/ThmeleafController.java
+++ ThymeleafSample/src/main/java/com/example/demo/controller/ThmeleafController.java
@@ -13,7 +13,7 @@ import com.example.demo.entity.Member;
 
 @Controller
 public class ThmeleafController {
-	
+
 	@GetMapping("show")
 	public String showView(Model model) {
 		// Memberを作成
@@ -21,32 +21,37 @@ public class ThmeleafController {
 		// コレクション買う膿瘍：Memberを作成
 		Member member1 = new Member(10, "田中");
 		Member member2 = new Member(20, "鈴木");
-		
+
 		// Listを作成
 		List<String> directionList = new ArrayList<>();
 		directionList.add("東");
 		directionList.add("西");
 		directionList.add("南");
 		directionList.add("北");
-		
+
 		// Mapを作成し、Memberを格納
 		Map<String, Member> memberMap = new HashMap<>();
 		memberMap.put("tanaka", member1);
 		memberMap.put("suzuki", member2);
-		
+
 		// Listを作成し、Memberを格納
 		List<Member> memberList = new ArrayList<>();
 		memberList.add(member1);
 		memberList.add(member2);
-		
+
 		// 「Model」にデータを格納する
 		model.addAttribute("name", "太郎");
 		model.addAttribute("mb", member);
 		model.addAttribute("list", directionList);
 		model.addAttribute("map", memberMap);
 		model.addAttribute("members", memberList);
-		
+
 		// 戻り値は「ビュー名」を返す
 		return "useThymeleaf";
+	}
+
+	@GetMapping("a")
+	public String showA() {
+		return "pageA";
 	}
 }
